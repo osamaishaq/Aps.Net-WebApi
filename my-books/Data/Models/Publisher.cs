7d441a99881140b70d32362523c91c9650d3ffe0
osama <osama.ishaq10@gmail.com>
2021-08-19 19:59:29 +0500

    Onr To Mnay Relationship added

---
+++ my-books/Data/Models/Publisher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace my_books.Data.Models
+{
+    public class Publisher
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        //Naviagtion Properties
+        public List<Book> Book { get; set; }
+    }
+}
